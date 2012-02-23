9c241843bbce80fc9edcad662f4feb34aa2cd33a
Justin Soliz <justinsoliz@gmail.com>
2012-02-23 15:58:33 -0800

    Initial commit

---
+++ JsonOutputDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CorporationWiki.Web.Common;
+
+namespace JsonOutputDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demoDate = new DateTime(2011, 2, 21);
+
+            var jsonVal = JsonSerializer.Current.SerializeObject(demoDate);
+
+            Console.WriteLine(jsonVal);
+        }
+    }
+}
